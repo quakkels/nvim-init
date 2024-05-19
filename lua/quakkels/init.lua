baa7ae272c33b69229ece395a581e495812a0982
Quakkels <quakkels@fastmail.com>
2024-05-18 20:00:00 -0500

    move everything

--- lua/quakkels/init.lua
+++ lua/quakkels/init.lua
@@ -1,13 +1,2 @@
-
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
+require("quakkels.set")
+require("quakkels.mapper")
