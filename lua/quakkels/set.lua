a266b607986c606fd060d3c364b1226c4d026386
Quakkels <quakkels@fastmail.com>
2024-05-20 21:00:45 -0500

    options

--- lua/quakkels/set.lua
+++ lua/quakkels/set.lua
@@ -9,3 +9,8 @@ vim.opt.expandtab = false
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.incsearch = true
+-- vim.opt.colorcolumn = "80"
+-- vim.cmd("hi ColorColumn ctermbg=1 guifg=#333333")
+
